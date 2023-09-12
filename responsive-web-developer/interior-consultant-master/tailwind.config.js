879dfb49dbaf10e3d24ed4ca367fca61f2e4d7b9
Xmorfin <bimosullivan10@gmail.com>
2023-09-12 22:18:18 +0700

    done

---
+++ responsive-web-developer/interior-consultant-master/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        lora: ["Lora"],
+        montserrat: ["Montserrat"],
+      },
+    },
+  },
+  plugins: [],
+};
